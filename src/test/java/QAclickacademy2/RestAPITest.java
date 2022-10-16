37813dca87178040880bd84a33fd0a0b897f5683
Sreya <sreyapaul24@gmail.com>
2022-10-16 12:18:21 +0530

    second commit

--- src/test/java/QAclickacademy2/RestAPITest.java
+++ src/test/java/QAclickacademy2/RestAPITest.java
@@ -7,6 +7,8 @@ public class RestAPITest {
 	public void testRestAPI1()
 	{
 		System.out.println("testRestAPI1");
+		System.out.println("testRestAPI12");
+		System.out.println("testRestAPI123");
 	}
 	@Test
 	public void testRestAPI2()
